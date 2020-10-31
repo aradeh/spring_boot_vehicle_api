e28f558e653a49488b3986e3737bb9ee83f5ed7a
Harshad Arade <harshadarade@MacBook-Pro.local>
2020-10-31 15:48:32 -0700

    pom changes to execute jar

--- src/main/java/com/restapi/vehicles/vehiclesapicrud/model/Vehicle.java
+++ src/main/java/com/restapi/vehicles/vehiclesapicrud/model/Vehicle.java
@@ -7,6 +7,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
 
+/**
+ * The type Vehicle.
+ *
+ * @author Harshad
+ */
 
 @Entity
 @Table( name = "vehicles" )
